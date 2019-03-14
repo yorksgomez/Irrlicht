489a1b270c67ca42b2c56458c479810a855757e0
Kevin <yorksgomez@gmail.com>
2019-03-14 12:57:56 -0500

    End of movement example

--- MyFirstApp.cpp
+++ MyFirstApp.cpp
@@ -7,81 +7,143 @@ a
 #include <irrlicht.h>
 #include "driverChoice.h"
 
-using namespace irr;
-
-using namespace video;
-using namespace scene;
-using namespace core;
-
 #ifdef _MSC_VER
-	#pragma comment(lib, "irrlicht.lib")
+#define _CRT_SECURE_NO_WARNINGS
+#pragma commet(lib, "Irrlicht.lib")
 #endif
 
-class MyEventReceiver : public IEventReceiver
-{
-	public:
-		virtual bool OnEvent(const SEvent& event) {
+using namespace irr;
 
-			if(event.EventType == irr::EET_KEY_INPUT_EVENT)
-				KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+class MyEventReceiver : public IEventReceiver {
+private:
+  bool KeyIsDown[KEY_KEY_CODES_COUNT];
 
-			return false;
-		}
+public:
+  MyEventReceiver() {
 
-		virtual bool IsKeyDown(EKEY_CODE keyCode) const {
-			return KeyIsDown[keyCode];
-		}
+    for(u32 i = 0; i < KEY_KEY_CODES_COUNT; ++i)
+      KeyIsDown[i] = false;
+    
+  }
+  
+  virtual bool OnEvent(const SEvent& event) {
 
-		MyEventReceiver() {
+    if(event.EventType == EET_KEY_INPUT_EVENT)
+      KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
 
-			for (u32 i = 0; i < KEY_KEY_CODES_COUNT; ++i)
-				KeyIsDown[i] = false;
+    return false;
+  }
 
-		}
-
-	private:
-		bool KeyIsDown[KEY_KEY_CODES_COUNT];
+   virtual bool IsKeyDown(EKEY_CODE keyCode) const
+    {
+        return KeyIsDown[keyCode];
+    }
+  
 };
 
 int main() {
-	E_DRIVER_TYPE driverType = driverChoiceConsole();
-	if(driverType == EDT_COUNT)
-		return 1;
+  video::E_DRIVER_TYPE driverType = driverChoiceConsole();
+  if(driverType == video::EDT_COUNT)
+    return 1;
 
-	MyEventReceiver receiver;
+  MyEventReceiver receiver;
 
-	IrrlichtDevice* device = createDevice(driverType, dimension2d<u32>(600, 480), 16, false, false, false, &receiver);
-	if(!device)
-		return 2;
+  IrrlichtDevice* device = createDevice(driverType, core::dimension2d<u32>(640, 480), 16, false, false, false, &receiver);
+  if(!device)
+    return 2;
 
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
+  video::IVideoDriver* driver = device->getVideoDriver();
+  scene::ISceneManager* smgr = device->getSceneManager();
 
-	ISceneNode* node = smgr->addSphereSceneNode();
-	if(node) {
-		node->setPosition(vector3df(0, 0, 30));
-		node->setMaterialTexture(0, driver->getTexture("media/wall.bmp"));
-		node->setMaterialFlag(EMF_LIGHTING, false);
-	}
+  scene::ISceneNode* node = smgr->addSphereSceneNode();
+  if(node) {
+    node->setPosition(core::vector3df(0, 0, 30));
+    node->setMaterialTexture(0, driver->getTexture("media/wall.bmp"));
+    node->setMaterialFlag(video::EMF_LIGHTING, false);
+  }
 
-	ISceneNode* n = smgr->addCubeSceneNode();
-	if(n) {
-		n->setMaterialTexture(0, driver->getTexture("media/t351sml.jpg"));
-		n->setMaterialFlag(EMF_LIGHTING, false);
-		ISceneNodeAnimator* anim = smgr->createFlyCircleAnimator(vector3df(0, 0, 30), 20.0f);
+  scene::ISceneNode* n = smgr->addCubeSceneNode();
 
-		if(anim) {
-			n->addAnimator(anim);
-			anim->drop();
-		}
+  if(n) {
+    n->setMaterialTexture(0, driver->getTexture("media/t351sml.jpg"));
+    n->setMaterialFlag(video::EMF_LIGHTING, false);
 
-	}
+    scene::ISceneNodeAnimator* anim = smgr->createFlyCircleAnimator(core::vector3df(0, 0, 30), 20.0f);
+    if(anim) {
+      n->addAnimator(anim);
+      anim->drop();
+    }
+  
+  }
 
-	while(device->run()) {
-		driver->beginScene(true, true, SColor(255, 113, 113, 133));
-		smgr->drawAll();
-		driver->endScene();
-	}
+  scene::IAnimatedMeshSceneNode* anms = smgr->addAnimatedMeshSceneNode(smgr->getMesh("media/ninja.b3d"));
+  if(anms) {
+    scene::ISceneNodeAnimator* anim = smgr->createFlyStraightAnimator(core::vector3df(100, 0, 60), core::vector3df(-100, 0, 60), 3500, true);
 
-	return 0;
+    if(anim) {
+      anms->addAnimator(anim);
+      anim->drop();
+    }
+
+    anms->setMaterialFlag(video::EMF_LIGHTING, false);
+
+    anms->setFrameLoop(0, 13);
+    anms->setAnimationSpeed(15);
+
+    anms->setScale(core::vector3df(2.f, 2.f, 2.f));
+    anms->setRotation(core::vector3df(0, -90, 0));    
+  }
+
+  smgr->addCameraSceneNodeFPS();
+  device->getCursorControl()->setVisible(false);
+
+  device->getGUIEnvironment()->addImage(driver->getTexture("media/irrlichtlogoalpha2.tga"), core::position2d<s32>(10, 20));
+
+  gui::IGUIStaticText* diagnostics = device->getGUIEnvironment()->addStaticText(L"", core::rect<s32>(10, 10, 400, 20));
+  diagnostics->setOverrideColor(video::SColor(255, 255, 255, 0));
+
+  int lastFPS = -1;
+  u32 then = device->getTimer()->getTime();
+  const f32 MOVEMENT_SPEED = 5.f;
+
+  while(device->run()) {
+    const u32 now = device->getTimer()->getTime();
+    const f32 frameDeltaTime = (f32)(now - then) / 1000.f;
+    then = now;
+
+    core::vector3df nodePosition = node->getPosition();
+
+    if(receiver.IsKeyDown(KEY_KEY_W))
+      nodePosition.Y += MOVEMENT_SPEED * frameDeltaTime;
+    else if(receiver.IsKeyDown(KEY_KEY_S))
+      nodePosition.Y -= MOVEMENT_SPEED * frameDeltaTime;
+
+    if(receiver.IsKeyDown(KEY_KEY_A))
+      nodePosition.X -= MOVEMENT_SPEED * frameDeltaTime;
+    else if(receiver.IsKeyDown(KEY_KEY_D))
+      nodePosition.X += MOVEMENT_SPEED * frameDeltaTime;
+
+    node->setPosition(nodePosition);
+
+    driver->beginScene(true, true, video::SColor(255, 113, 113, 133));
+
+    smgr->drawAll();
+    device->getGUIEnvironment()->drawAll();
+
+    driver->endScene();
+
+    int fps = driver->getFPS();
+
+    if(lastFPS != fps) {
+      core::stringw tmp(L"Movement example [");
+      tmp += driver->getName();
+      tmp += L"] FPS: [";
+      tmp += fps;
+      tmp += L"]";
+    }	    
+      
+  }
+
+  device->drop();
+  return 0;
 }
